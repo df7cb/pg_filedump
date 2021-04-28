c3f076fd86443ff18c791f440ff5cf1bd88bf686
Richard Yen <richard.yen@enterprisedb.com>
2021-04-28 15:37:29 -0700

    Add support for decoding pg_filenode.map

--- pg_filedump.h
+++ pg_filedump.h
@@ -137,6 +137,14 @@ typedef enum optionReturnCodes
 #define EOF_ENCOUNTERED (-1)	/* Indicator for partial read */
 #define BYTES_PER_LINE 16		/* Format the binary 16 bytes per line */
 
+/* Constants for pg_relnode.map decoding */
+#define RELMAPPER_MAGICSIZE   4
+#define RELMAPPER_FILESIZE    512
+/* From utils/cache/relmapper.c -- Maybe ask community to put
+ * these into utils/cache/relmapper.h? */
+#define RELMAPPER_FILEMAGIC   0x592717
+#define MAX_MAPPINGS          62
+
 extern char *fileName;
 
 /*
