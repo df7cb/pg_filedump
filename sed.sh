06918e9c46022cfe056cc584d734cec9371693a8
Christoph Berg <myon@debian.org>
2025-04-15 12:56:48 +0000

    Mask all digits of value and toast relation ids
    This was not noticed before because I was always running the tests in a
    fresh instance.
    
    Close #33.
    
    Author: Svetlana Derevyanko <s.derevyanko@postgrespro.ru>

--- sed.sh
+++ sed.sh
@@ -3,5 +3,5 @@
 sed -e "s/logid ....../logid ....../" \
     -e "s/recoff 0x......../recoff 0x......../" \
     -e "s/Checksum: 0x..../Checksum: 0x..../" \
-    -e "s/id:  ...../id:  ...../g" \
+    -e "s/id: ....../id: ....../g" \
     -e "s/ 8< .*/ 8< [snipped]/"
